90b84152517e473124a09d97a00523c687e53c98
Siddharth Singh <me@shantaram.xyz>
2023-09-23 01:48:58 +0530

    git history unbork

--- id_verification/verification.py
+++ id_verification/verification.py
@@ -5,9 +5,16 @@ import base64
 import numpy as np
 from typing import List
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
 
 app = fastapi.FastAPI()
-THRESHOLD = 0.6
+THRESHOLD = 0.5
+
+tester_html = open("api_tester.html").read()
+
+@app.get("/tester")
+async def tester():
+    return HTMLResponse(content=tester_html, status_code=200)
 
 class VerifyRequest(BaseModel):
     id_img: str
@@ -18,16 +25,26 @@ async def verify_id(req: VerifyRequest):
     id_img = readb64(req.id_img)
     live_img = readb64(req.face_img)
 
-    id_bounds = extract_face_from_image(id_img)
-    live_bounds = extract_face_from_image(live_img)
+    id_face = extract_face_from_image(id_img)
+    live_face = extract_face_from_image(live_img)
 
-    if (id_bounds is None or live_bounds is None):
+    if (id_face is None or live_face is None):
         return {
             "error": "Bad Image(s)"
         }
-    
+
+    # Determine the target size for scaling (e.g., 100x100 pixels)
+    target_size = (200, 200)
+
+    id_to_scale = np.array(id_face, dtype='uint8')
+    live_to_scale = np.array(live_face, dtype='uint8')
+    # Resize both cropped images to the target size
+    scaled_id = cv2.resize(id_to_scale, target_size)
+    scaled_live = cv2.resize(live_to_scale, target_size)
+
+    result = compare_faces(scaled_id, scaled_live)
     return {
-        "verified": compare_faces(id_img, id_bounds, live_img, live_bounds)
+        "verified": result
     }
 
 
@@ -43,33 +60,23 @@ def extract_face_from_image(student_img):
     faces = face_cascade.detectMultiScale(
         gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Iterate through the detected faces and draw rectangles around them
-    (x, y, w, h) = faces[0]
-
     # If you want to extract and return the first detected face:
     if len(faces) >= 1:
-        first_face = faces[0]
-        x, y, w, h = first_face
-        extracted_face = student_img[y:y + h, x:x + w]
-        return extracted_face
+        x, y, w, h = faces[0]
+        extracted_face = gray_image[y:y + h, x:x + w]
+        return cv2.cvtColor(extracted_face, cv2.COLOR_GRAY2BGR)
 
 def readb64(uri):
    encoded_data = uri.split(',')[1]
    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-   print(type(img))
    return img
 
-def compare_faces(from_id, id_bounds, from_live, live_bounds):
+def compare_faces(from_id, from_live):
     # Load the known face encoding (extracted from the student ID)
-    known_face_encoding = face_recognition.face_encodings(from_id, known_face_locations=[id_bounds])  # Assuming one face is detected
-    live_face_encoding = face_recognition.face_encodings(from_live, known_face_locations=[live_bounds])
+    known_face_encodings = face_recognition.face_encodings(from_id)
+    live_face_encoding = face_recognition.face_encodings(from_live)[0]
 
-    face_distance = face_recognition.face_distance([known_face_encoding], live_face_encoding)
-
-    # Set a threshold for similarity (e.g., 0.6) and check if the face matches
-    if face_distance[0] < THRESHOLD:
-        # Face in video feed matches the student's face
-        # Take appropriate actions or display the result
-        return True
-    return False
+    results = face_recognition.compare_faces(known_face_encodings, live_face_encoding, tolerance=0.45)
+    print(results)
+    return bool(results[0])
