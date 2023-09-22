74380d1bc3326fbe400ecc21a974bf59b01d0324
Sreekar <saisreekar2713@gmail.com>
2023-09-22 21:13:37 +0530

    comparing py

--- id_verification/verification.py
+++ id_verification/verification.py
@@ -17,7 +17,9 @@ async def verify_id(id_img: str, face_img: str):
             "error": "Bad Image"
         }
     
-    return compare_faces(from_id, from_live)
+    return {
+        "verified": compare_faces(from_id, from_live)
+    }
 
 
 def extract_face_from_image(student_img):
@@ -46,21 +48,14 @@ def readb64(uri):
 
 def compare_faces(from_id, from_live):
     # Load the known face encoding (extracted from the student ID)
-    known_face_encoding = face_recognition.face_encodings(
-        detected_face)[0]  # Assuming one face is detected
-
-    # Encode the faces in the video frames
-    face_locations = face_recognition.face_locations(frame)
-    face_encodings = face_recognition.face_encodings(frame, face_locations)
-
-    # Compare the face encoding from the student ID with the face encodings from the video frames
-    for encoding in face_encodings:
+    known_face_encoding = face_recognition.face_encodings(from_id)[0]  # Assuming one face is detected
+    live_face_encoding = face_recognition.face_encodings(from_live)[0]
         # Compare the encodings using a distance metric (e.g., Euclidean distance)
-        face_distance = face_recognition.face_distance(
-            [known_face_encoding], encoding)
+    face_distance = face_recognition.face_distance([known_face_encoding], live_face_encoding)
 
-        # Set a threshold for similarity (e.g., 0.6) and check if the face matches
-        if face_distance[0] < 0.6:
-            # Face in video feed matches the student's face
-            # Take appropriate actions or display the result
-            print("Match found")
+    # Set a threshold for similarity (e.g., 0.6) and check if the face matches
+    if face_distance[0] < THRESHOLD:
+        # Face in video feed matches the student's face
+        # Take appropriate actions or display the result
+        return True
+    return False
